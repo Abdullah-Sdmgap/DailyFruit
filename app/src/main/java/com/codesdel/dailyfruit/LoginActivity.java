a2d447b5c8716be771e25006d681c3a3b151e402
Ahm Abdullah <Abdullah.sdmgap@gmail.com>
2019-12-22 13:19:42 +0600

    Login Activity Design Complete

---
+++ app/src/main/java/com/codesdel/dailyfruit/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.codesdel.dailyfruit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/codesdel/dailyfruit/MainActivity.java
+++ app/src/main/java/com/codesdel/dailyfruit/MainActivity.java
@@ -2,7 +2,9 @@ package com.codesdel.dailyfruit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
@@ -17,5 +19,13 @@ public class MainActivity extends AppCompatActivity
 
         joinNowButton = (Button)findViewById(R.id.main_join_now_btn);
         loginButton = (Button)findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
