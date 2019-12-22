f69a9df626978f70bb104e61dc6b625cd95798b1
Ahm Abdullah <Abdullah.sdmgap@gmail.com>
2019-12-22 12:38:07 +0600

    Home Activity finished

--- app/src/main/java/com/codesdel/dailyfruit/MainActivity.java
+++ app/src/main/java/com/codesdel/dailyfruit/MainActivity.java
@@ -3,12 +3,19 @@ package com.codesdel.dailyfruit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button joinNowButton, loginButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button)findViewById(R.id.main_join_now_btn);
+        loginButton = (Button)findViewById(R.id.main_login_btn);
     }
 }
