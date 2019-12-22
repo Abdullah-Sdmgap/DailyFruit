3e2b08cc8de0d10a6c1aeb184da9c7b1fe02e2be
Ahm Abdullah <Abdullah.sdmgap@gmail.com>
2019-12-22 11:17:27 +0600

    First Test Commit

---
+++ app/src/main/java/com/codesdel/dailyfruit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codesdel.dailyfruit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
